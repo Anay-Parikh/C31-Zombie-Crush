8303cbcab6d59c6b5df405bdce6b2a10892c1e12
UnknownSnake <86217333+Anay-Parikh@users.noreply.github.com>
2021-08-24 17:57:11 -0700

    Update Base.js

--- js/Base.js
+++ js/Base.js
@@ -1,1 +1,25 @@
+class Base {
+    constructor(x, y, w, h) {
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.image = loadImage("./assets/wall.png");
+        this.w = w;
+        this.h = h;
+        Composite.add(world, this.body);
+    }
 
+    display() {
+        var pos = this.body.position;
+
+        push();
+        //translate(pos.x, pos.y);
+        //fill("#ad6e3b")
+        //rectMode(CENTER);
+        imageMode(CENTER);
+        image(this.image, pos.x, pos.y, this.w, this.h);
+        //rect(pos.x, pos.y, this.w, this.h);
+        pop();
+    }
+}
